40f45f0ded334981909ed67925e745876ce8ade0
Cem Erkan Kilinc <cemkilinc@gmail.com>
2023-05-12 18:59:25 +0300

    Packages added to list, randomly appear one by one, last one is red

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -10,27 +10,90 @@ public class Delivery : MonoBehaviour
         [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
         [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
 
-        SpriteRenderer spriteRenderer;
+        //[SerializeField] List<GameObject> packageList = new List<GameObject>();
+        [SerializeField] GameObject allPackages;
+
+        List<GameObject> packageList;
+
+        SpriteRenderer vehicleSpriteRenderer;
+
+        int currentIndex;
+
+        int nextIndex;
+
+        List<int> usedPackages = new List<int>();
+
 
         void Start() 
         {
-            spriteRenderer = GetComponent<SpriteRenderer>();
+            vehicleSpriteRenderer = GetComponent<SpriteRenderer>();
+
+            packageList = new List<GameObject>();
+
+            foreach (Transform package in allPackages.transform)
+            {
+                packageList.Add(package.gameObject);
+            }
+            for (int i = 0; i < packageList.Count; i++)
+            {
+                packageList[i].SetActive(false);
+            }
+            
+            currentIndex = Random.Range(0, packageList.Count);
+            packageList[currentIndex].SetActive(true);
+            usedPackages.Add(currentIndex);
+            
         }
 
         void OnTriggerEnter2D(Collider2D other) 
         {
             if (other.tag == "Package" && !hasPackage)
             {
+                /*
+                foreach (GameObject package in packageList)
+                {
+                    package.SetActive(false);
+                }
+                */
+                /*
+                for (int i = 0; i < packageList.Count; i++)
+                {
+                    packageList[i].SetActive(false);
+                }
+                */
                 Debug.Log("Package picked up.");
                 hasPackage = true;
-                spriteRenderer.color = hasPackageColor;
-                Destroy(other.gameObject,destroyDelay);
+                vehicleSpriteRenderer.color = hasPackageColor;
+                //Destroy(other.gameObject,destroyDelay);
+                do
+                {
+                        nextIndex = Random.Range(0,packageList.Count);
+                }
+                while (usedPackages.Contains(nextIndex) && usedPackages.Count < packageList.Count);
+
+                if (usedPackages.Count == packageList.Count-1)
+                    {
+                        SpriteRenderer packageSpriteRenderer = packageList[nextIndex].GetComponent<SpriteRenderer>();
+                        packageSpriteRenderer.color = Color.red;
+                    }
+                if (usedPackages.Count == packageList.Count)
+                {
+                    vehicleSpriteRenderer.color = Color.red;
+                }
+                packageList[currentIndex].SetActive(false);
             }
             else if (other.tag == "Customer" && hasPackage)
             {
+                Debug.Log(packageList.Count.ToString());
                 Debug.Log("Package delivered.");
                 hasPackage = false;
-                spriteRenderer.color = noPackageColor;
+                vehicleSpriteRenderer.color = noPackageColor;
+                if (usedPackages.Count < packageList.Count)
+                {
+                    packageList[nextIndex].SetActive(true);
+                }
+                usedPackages.Add(nextIndex);
+                currentIndex = nextIndex;
             }
         }
 }
