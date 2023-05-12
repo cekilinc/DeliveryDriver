545b4592e5f901b9bcde54b7ca0e2b12bab17d2a
Cem Erkan Kilinc <cemkilinc@gmail.com>
2023-05-12 22:46:33 +0300

    Countdown added to start, movespeed set to 0 before

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,23 +6,52 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float normalSpeed = 10f;
     [SerializeField] float steeringSpeed = 200f;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 0;
     [SerializeField] float slowSpeed = 5f;
     [SerializeField] float boostSpeed = 20f;
 
     [SerializeField] float slowTime = 2f;
     
+    /* void setNormalSpeed ()
+    {
+        moveSpeed = normalSpeed;
+    } */
+
+    void Start() 
+    {
+        StartCoroutine(Countdown(3));
+        //below does the same job
+        //StartCoroutine("Countdown",3);
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         moveSpeed = slowSpeed;
+        //Invoke starts after specified time
+        //Invoke("setNormalSpeed",slowTime);
+
+
+        //You can pass arguments to a coroutine
         StartCoroutine(ResetMoveSpeed(slowTime));
     }
+
+    IEnumerator Countdown (int seconds)
+    {
+        while (seconds > 0)
+        {
+            Debug.Log(seconds.ToString());
+            seconds--;
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("Go!");
+        moveSpeed = normalSpeed;
+    }    
 
     IEnumerator ResetMoveSpeed(float slowTime)
     {
         yield return new WaitForSeconds(slowTime);
         moveSpeed = normalSpeed;
-    }
+    } 
 
     void OnTriggerEnter2D(Collider2D other) 
     {
