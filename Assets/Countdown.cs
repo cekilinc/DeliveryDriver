f03f4932004acdfcfefb48ba298c34fdc241ceda
Cem Erkan Kilinc <cemkilinc@gmail.com>
2023-05-23 18:22:21 +0300

    Countdown added in game

--- Assets/Countdown.cs
+++ Assets/Countdown.cs
@@ -5,18 +5,37 @@ using TMPro;
 
 public class Countdown : MonoBehaviour
 {
+
+    [SerializeField] float totalTime = 300f;
+    float remainingTime;
+
     public TMP_Text countdown;
+    bool gameCountdown;
     
     // Start is called before the first frame update
     void Start()
     {
         countdown = GetComponent<TextMeshProUGUI>();
         StartCoroutine(startCountdown(3));
+        remainingTime = totalTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int minutes = (int)(remainingTime/60);
+        int seconds = (int)(remainingTime%60);
+        int miliseconds = (int)((remainingTime*10)%10);
+        if (gameCountdown)
+        {
+            remainingTime -= Time.deltaTime;
+            countdown.text = string.Format("{0:0}:{1:00}:{2:0}",minutes,seconds,miliseconds);
+        }
+        if(remainingTime <= 0)
+        {
+            countdown.text = "TIME'S UP!";
+            gameCountdown = false;
+        }
         
     }
 
@@ -34,7 +53,8 @@ public class Countdown : MonoBehaviour
             }
             seconds--;
             yield return new WaitForSeconds(1f);
-            countdown.text ="";
         }
+        countdown.text ="";
+        gameCountdown = true;
     }
 }
